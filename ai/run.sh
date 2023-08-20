02781fc7a91b3407df8eba1179cbbe8fcd10d38f
Chop TRAN <m@chop.dev>
2023-08-20 16:54:06 +0700

    chores

--- ai/run.sh
+++ ai/run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-gunicorn -k uvicorn.workers.UvicornWorker --bind :$PORT --workers 1 --threads 8 --timeout 0 --chdir src main:app
+gunicorn -k uvicorn.workers.UvicornWorker \
+    --bind :$PORT \
+    --workers 1 \
+    --threads 8 \
+    --timeout 0 \
+    --chdir src \
+    main:app
